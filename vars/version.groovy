7814fa88c57417a77d708d8e2af22d3bd0e23328
David Schott <schottdb@gmail.com>
2017-10-07 19:01:23 -0400

    fixed a bug

--- vars/version.groovy
+++ vars/version.groovy
@@ -1,4 +1,6 @@
 def call(body) {
-    return sh returnStdout: true,
-	    script: "echo ${BUILD_NUMBER}-`git rev-parse HEAD` | tr -d '\n'"
+    def version = sh returnStdout: true,
+	    script: "echo ${env.BUILD_NUMBER}-`git rev-parse HEAD` | tr -d '\n'"
+	echo "Set version to ${version}"
+    return version
 }
