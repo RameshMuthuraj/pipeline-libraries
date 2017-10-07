3d79ddc070643a3653b74fa62993bfe366bf0981
David Schott <schottdb@gmail.com>
2017-10-07 19:20:16 -0400

    s/version/dockerImageTag

--- vars/version.groovy
+++
@@ -1,6 +0,0 @@
-def call(body) {
-    def version = sh returnStdout: true,
-	    script: "echo ${env.BUILD_NUMBER}-`git rev-parse HEAD` | tr -d '\n'"
-	echo "Set version to ${version}"
-    return version
-}
