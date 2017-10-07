565f50d50ccaaf68b6eda4a7d786dc1f6dce90d9
David Schott <schottdb@gmail.com>
2017-10-07 19:30:12 -0400

    new global var - dockerNuke

--- vars/dockerNuke.groovy
+++ vars/dockerNuke.groovy
@@ -1,3 +1,8 @@
 def call(imageName, imageTag) {
-    
+    //attempts to stop container, remove the container, remove the image
+    sh """
+	    docker ps -q --filter ancestor='$imageName:$imageTag' | xargs docker stop || true
+		docker ps -a -q --filter ancestor='$imageName:$imageTag' | xargs docker rm || true
+		docker images --format '{{.Repository}}:{{.Tag}}' | grep '^$imageName' | xargs docker rmi || true
+	"""
 }
